a5e710686f9ec72af542dc4da8840f7f82357a09
srikargunnam <i@srikargunnam.com>
2024-11-01 10:38:32 +0530

    chore: configure expo fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,23 @@
 import { Stack } from "expo-router";
 import "../../global.css";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [] = useFonts({
+    "OpenSans-Bold": require("../../assets/fonts/OpenSans-Bold.ttf"),
+    "OpenSans-BoldItalic": require("../../assets/fonts/OpenSans-BoldItalic.ttf"),
+    "OpenSans-ExtraBold": require("../../assets/fonts/OpenSans-ExtraBold.ttf"),
+    "OpenSans-ExtraBoldItalic": require("../../assets/fonts/OpenSans-ExtraBoldItalic.ttf"),
+    "OpenSans-Italic": require("../../assets/fonts/OpenSans-Italic.ttf"),
+    "OpenSans-Light": require("../../assets/fonts/OpenSans-Light.ttf"),
+    "OpenSans-LightItalic": require("../../assets/fonts/OpenSans-LightItalic.ttf"),
+    "OpenSans-Medium": require("../../assets/fonts/OpenSans-Medium.ttf"),
+    "OpenSans-MediumItalic": require("../../assets/fonts/OpenSans-MediumItalic.ttf"),
+    "OpenSans-Regular": require("../../assets/fonts/OpenSans-Regular.ttf"),
+    "OpenSans-SemiBold": require("../../assets/fonts/OpenSans-SemiBold.ttf"),
+    "OpenSans-SemiBoldItalic": require("../../assets/fonts/OpenSans-SemiBoldItalic.ttf"),
+  });
+
   const isSignedIn = false; // Placeholder for authentication state, replace with jotai atom
   const isOnboardingCompleted = false; // Placeholder for onboarding state, replace with jotai atom
 
