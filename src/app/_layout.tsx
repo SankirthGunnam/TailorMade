25169aa565f70165eded2ec125e8a50a27a98b71
srikargunnam <i@srikargunnam.com>
2024-11-01 12:58:42 +0530

    fix: stack issues and prebuild

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -19,34 +19,20 @@ export default function RootLayout() {
   });
 
   const isSignedIn = false; // Placeholder for authentication state, replace with jotai atom
-  const isOnboardingCompleted = false; // Placeholder for onboarding state, replace with jotai atom
 
   return (
-    <>
+    <Stack
+      initialRouteName={isSignedIn ? "authStack" : "unAuthStack"}
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       {isSignedIn ? (
-        <Stack
-          initialRouteName="index"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#800020", // Deep Burgundy for header background
-            },
-            headerTitleStyle: {
-              color: "#FFFFF0", // Ivory for header title
-            },
-          }}
-        >
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="cart" />
-          <Stack.Screen name="+not-found" />
-          <Stack.Screen name="open-camera" options={{ headerShown: false }} />
-          <Stack.Screen name="show-measurements" />
-        </Stack>
+        <Stack.Screen name="authStack" />
       ) : (
-        <Stack initialRouteName={isOnboardingCompleted ? "login" : "index"}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="login" />
-        </Stack>
+        <Stack.Screen name="unAuthStack" />
       )}
-    </>
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 }
---
+++ src/app/authStack/index.tsx
@@ -0,0 +1,7 @@
+import Home from "../../screens/home";
+
+const HomeLayout = () => {
+  return <Home />;
+};
+
+export default HomeLayout;
