51c39e66b57272975b13efe47ff09305c5d8e0cf
srikargunnam <i@srikargunnam.com>
2024-11-01 12:58:42 +0530

    fix: stack issues and prebuild

---
+++ src/app/index.tsx
@@ -0,0 +1,18 @@
+import { router } from "expo-router";
+import { useEffect } from "react";
+import { View } from "react-native";
+
+export default function Index() {
+  const isSignedIn = false; // Placeholder for authentication state, replace with jotai atom
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      isSignedIn
+        ? router.navigate("/authStack")
+        : router.navigate("/unAuthStack");
+    }, 0);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return <View className="h-full w-full bg-primary-burgundy" />;
+}
